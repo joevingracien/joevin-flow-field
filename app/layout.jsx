ada5a5b46604f8f3fad7d563445971ea66f860c6
joBar <jg.getintouch@gmail.com>
2024-01-23 21:02:05 +0100

    div space

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,9 +14,7 @@ export default function RootLayout({ children }) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
-        <Layout>{children}</Layout>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
