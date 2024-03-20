143473adc08fe624aa0e0a04dc3bdc0417747963
joBar <jg.getintouch@gmail.com>
2024-03-20 21:45:14 +0100

    zzzz

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,15 +12,15 @@ export const metadata = {
 }
 
 // Font files can be colocated inside of `app`
-const HelveticaNow = localFont({
+/* const HelveticaNow = localFont({
   src: '../public/fonts/HelveticaNowVariable.woff2',
   variable: '--font-helvetica-now-var',
   display: 'swap',
-})
+}) */
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className={HelveticaNow.variable}>
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,39 @@
-// tailwind.config.js
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  // ...
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-helvetica-now-var)'],
+        sans: ['var(--font-website)', ...fontFamily.sans],
+      },
+      fontVariationSettings: {
+        'text-bold': {
+          opsz: 12,
+          wght: 700,
+          wdth: 100,
+        },
+        'text-light': {
+          opsz: 12,
+          wght: 300,
+          wdth: 100,
+        },
+        'text-normal': {
+          opsz: 12,
+          wght: 400,
+          wdth: 100,
+        },
+        'display-black': {
+          opsz: 30,
+          wght: 900,
+          wdth: 50,
+        },
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
