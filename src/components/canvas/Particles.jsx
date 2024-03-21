b88ffd04ab5ef829b4a76e2d114a3fe22bc0896a
joBar <jg.getintouch@gmail.com>
2024-03-21 19:32:27 +0100

    zz

--- src/components/canvas/Particles.jsx
+++ src/components/canvas/Particles.jsx
@@ -90,7 +90,7 @@ export function Particles() {
         <sphereGeometry args={[0.1, 32, 32]} />
         <meshBasicMaterial transparent opacity={0} />
       </mesh>
-      <points position={[1.5, -1.1, 0]}>
+      <points>
         <bufferGeometry>
           <bufferAttribute attach='attributes-position' count={particles.length / 3} array={particles} itemSize={3} />
           <bufferAttribute attach='attributes-ref' count={ref.length / 3} array={ref} itemSize={2} />
