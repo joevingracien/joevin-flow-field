3080573834702494bbb36565d55b0d2e5e82579e
Joevin <jg.getintouch@gmail.com>
2024-06-18 12:49:41 +0200

    no dot

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,7 @@ const Home = () => {
       </header>
       <section className='flex h-[90vh]'>
         <div className=' -mt-32 flex max-w-[25rem] flex-col justify-center gap-6'>
-          <h1 className='font-sans text-3xl font-bold text-white'>Elevating web experiences.</h1>
+          <h1 className='font-sans text-3xl font-bold text-white'>Elevating web experiences</h1>
           <p className=' '>
             Greetings from the digital space! <br /> I'm Joevin, your partner in propelling user-centered websites to
             new heights.
