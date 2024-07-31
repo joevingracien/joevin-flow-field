a2b574e9b0f88c849b04e11c4ad8adc299253ec6
Joevin <jg.getintouch@gmail.com>
2024-07-31 15:56:22 +0200

    mouse back

--- src/components/canvas/SimulationMaterial.jsx
+++ src/components/canvas/SimulationMaterial.jsx
@@ -35,30 +35,29 @@ const SimulationMaterial = shaderMaterial(
       vec2 original = texture2D(uOriginalPosition, vUv).xy;
       vec2 velocity = texture2D(uPosition, vUv).zw;
 
-      velocity *= 0.95;
+      velocity *= 0.99;
+
+      // Non-linear acceleration factor with longer slow start
+      float t = clamp(uTime * 0.1, 0.0, 1.0); // Adjust the 0.1 to control overall speed
+      float accelerationFactor = easeInOutQuint(t);
 
       // Calculate target position (original position)
       vec2 targetPosition = original;
-
-      // Non-linear acceleration factor with longer slow start
-      float t = clamp(uTime * 0.2, 0.0, 1.0); // Adjust the 0.2 to control overall speed
-      float accelerationFactor = easeInOutQuint(t);
 
       // Apply non-linear acceleration to movement towards original position
       vec2 direction = targetPosition - position;
       float distanceToTarget = length(direction);
       
       // Increase speed based on acceleration factor and distance
-      float speed = mix(0.001, 0.1, accelerationFactor) * distanceToTarget;
+      float speed = mix(0.0001, 0.1, accelerationFactor) * distanceToTarget;
       position += normalize(direction) * speed;
 
-      // Mouse repel force
+      // Mouse repel force (original implementation)
       float mouseDistance = distance(position, uMouse.xy);
       float maxDistance = 0.4;
       if (mouseDistance < maxDistance) {
-        vec2 repelDirection = normalize(position - uMouse.xy);
-        float repelStrength = (1.0 - accelerationFactor) * 0.01; // Reduce mouse influence over time
-        velocity += repelDirection * (1.0 - mouseDistance / maxDistance) * repelStrength;
+        vec2 direction = normalize(position - uMouse.xy);
+        velocity += direction * (1.0 - mouseDistance / maxDistance) * 0.01;
       }
 
       position.xy += velocity;
