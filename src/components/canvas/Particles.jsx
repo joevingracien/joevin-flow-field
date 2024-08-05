90793609f0ebc500803515bb903fef30b52d73e8
Joevin <jg.getintouch@gmail.com>
2024-08-05 17:51:57 +0200

    optimize test

--- src/components/canvas/Particles.jsx
+++ src/components/canvas/Particles.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useMemo, useCallback } from 'react'
 import './RenderMaterial'
 import './SimulationMaterial'
 import { getDataTexture } from './getDataTexture'
@@ -10,82 +10,102 @@ import { useThree } from '@react-three/fiber'
 import { useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three'
 
+const SIZE = 512
+
 export function Particles() {
-  const SIZE = 512
   const photoTexture = useLoader(TextureLoader, '/img/photospaceme.webp')
 
-  const particles = new Float32Array(SIZE * SIZE * 3)
-  for (let i = 0; i < SIZE; i++) {
-    for (let j = 0; j < SIZE; j++) {
-      const k = i * SIZE + j
-      particles[k * 3 + 0] = (5 * i) / SIZE
-      particles[k * 3 + 1] = (5 * j) / SIZE
-      particles[k * 3 + 2] = 0
+  const particles = useMemo(() => {
+    const p = new Float32Array(SIZE * SIZE * 3)
+    for (let i = 0; i < SIZE; i++) {
+      for (let j = 0; j < SIZE; j++) {
+        const k = i * SIZE + j
+        p[k * 3 + 0] = (5 * i) / SIZE
+        p[k * 3 + 1] = (5 * j) / SIZE
+        p[k * 3 + 2] = 0
+      }
     }
-  }
+    return p
+  }, [])
 
-  const ref = new Float32Array(SIZE * SIZE * 2)
-  for (let i = 0; i < SIZE; i++) {
-    for (let j = 0; j < SIZE; j++) {
-      const k = i * SIZE + j
-      ref[k * 2 + 0] = i / (SIZE - 1)
-      ref[k * 2 + 1] = j / (SIZE - 1)
+  const ref = useMemo(() => {
+    const r = new Float32Array(SIZE * SIZE * 2)
+    for (let i = 0; i < SIZE; i++) {
+      for (let j = 0; j < SIZE; j++) {
+        const k = i * SIZE + j
+        r[k * 2 + 0] = i / (SIZE - 1)
+        r[k * 2 + 1] = j / (SIZE - 1)
+      }
     }
-  }
-  const scene = new THREE.Scene()
-  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1)
-  let target0 = useFBO(SIZE, SIZE, {
-    magFilter: THREE.NearestFilter,
-    minFilter: THREE.NearestFilter,
-    type: THREE.FloatType,
-  })
-  let target1 = useFBO(SIZE, SIZE, {
-    magFilter: THREE.NearestFilter,
-    minFilter: THREE.NearestFilter,
-    type: THREE.FloatType,
-  })
+    return r
+  }, [])
+
+  const scene = useMemo(() => new THREE.Scene(), [])
+  const camera = useMemo(() => new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1), [])
+
+  const [target0, target1] = useMemo(
+    () => [
+      useFBO(SIZE, SIZE, {
+        magFilter: THREE.NearestFilter,
+        minFilter: THREE.NearestFilter,
+        type: THREE.FloatType,
+      }),
+      useFBO(SIZE, SIZE, {
+        magFilter: THREE.NearestFilter,
+        minFilter: THREE.NearestFilter,
+        type: THREE.FloatType,
+      }),
+    ],
+    [],
+  )
+
   const simMat = useRef()
   const renderMat = useRef()
   const followMouse = useRef()
 
   const { viewport, size } = useThree()
 
-  const originalPosition = getDataTexture(SIZE)
+  const originalPosition = useMemo(() => getDataTexture(SIZE), [])
 
   const mouse = useRef(new THREE.Vector2(0, 0))
   const isTouch = useRef(false)
   const isTouchActive = useRef(false)
 
+  const updateMousePosition = useCallback(
+    (clientX, clientY) => {
+      mouse.current.x = (clientX / size.width) * 2 - 1
+      mouse.current.y = -(clientY / size.height) * 2 + 1
+    },
+    [size],
+  )
+
   useEffect(() => {
     const handleMouseMove = (event) => {
       if (!isTouch.current) {
-        mouse.current.x = (event.clientX / size.width) * 2 - 1
-        mouse.current.y = -(event.clientY / size.height) * 2 + 1
+        updateMousePosition(event.clientX, event.clientY)
       }
     }
 
     const handleTouchMove = (event) => {
       if (isTouchActive.current) {
-        mouse.current.x = (event.touches[0].clientX / size.width) * 2 - 1
-        mouse.current.y = -(event.touches[0].clientY / size.height) * 2 + 1
+        updateMousePosition(event.touches[0].clientX, event.touches[0].clientY)
       }
     }
 
     const handleTouchStart = (event) => {
       isTouch.current = true
       isTouchActive.current = true
-      mouse.current.x = (event.touches[0].clientX / size.width) * 2 - 1
-      mouse.current.y = -(event.touches[0].clientY / size.height) * 2 + 1
+      updateMousePosition(event.touches[0].clientX, event.touches[0].clientY)
     }
 
     const handleTouchEnd = () => {
       isTouchActive.current = false
     }
 
-    window.addEventListener('mousemove', handleMouseMove)
-    window.addEventListener('touchmove', handleTouchMove)
-    window.addEventListener('touchstart', handleTouchStart)
-    window.addEventListener('touchend', handleTouchEnd)
+    window.addEventListener('mousemove', handleMouseMove, { passive: true })
+    window.addEventListener('touchmove', handleTouchMove, { passive: true })
+    window.addEventListener('touchstart', handleTouchStart, { passive: true })
+    window.addEventListener('touchend', handleTouchEnd, { passive: true })
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
@@ -93,15 +113,17 @@ export function Particles() {
       window.removeEventListener('touchstart', handleTouchStart)
       window.removeEventListener('touchend', handleTouchEnd)
     }
-  }, [size])
+  }, [updateMousePosition])
 
   useFrame(() => {
     if (followMouse.current && (!isTouch.current || isTouchActive.current)) {
-      followMouse.current.position.x = (mouse.current.x * viewport.width) / 2
-      followMouse.current.position.y = (mouse.current.y * viewport.height) / 2
+      const mouseX = (mouse.current.x * viewport.width) / 2
+      const mouseY = (mouse.current.y * viewport.height) / 2
 
-      simMat.current.uniforms.uMouse.value.x = (mouse.current.x * viewport.width) / 2
-      simMat.current.uniforms.uMouse.value.y = (mouse.current.y * viewport.height) / 2
+      followMouse.current.position.x = mouseX
+      followMouse.current.position.y = mouseY
+
+      simMat.current.uniforms.uMouse.value.set(mouseX, mouseY)
     }
   })
 
@@ -109,10 +131,12 @@ export function Particles() {
     gl.setRenderTarget(target0)
     gl.render(scene, camera)
     gl.setRenderTarget(null)
+
     renderMat.current.uniforms.uPosition.value = target1.texture
     simMat.current.uniforms.uPosition.value = target0.texture
 
-    let temp = target0
+    // Swap render targets
+    const temp = target0
     target0 = target1
     target1 = temp
   })
@@ -138,7 +162,7 @@ export function Particles() {
       <points>
         <bufferGeometry>
           <bufferAttribute attach='attributes-position' count={particles.length / 3} array={particles} itemSize={3} />
-          <bufferAttribute attach='attributes-ref' count={ref.length / 3} array={ref} itemSize={2} />
+          <bufferAttribute attach='attributes-ref' count={ref.length / 2} array={ref} itemSize={2} />
         </bufferGeometry>
         <renderMaterial transparent={true} blending={THREE.AdditiveBlending} ref={renderMat} uTexture={photoTexture} />
       </points>
